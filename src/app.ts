a5416d85e0944fe8cd028b2110f75fc2b1a2dc92
modikrunal202 <email>
2021-10-05 21:13:58 +0530

    commit 2

--- src/app.ts
+++ src/app.ts
@@ -43,6 +43,7 @@ const main = async () => {
   } catch (error) {
     console.error(error);
     console.log("Commit 1")
+    console.log("Commit 2")
     throw new Error("Unable to connect to db");
   }
 };
